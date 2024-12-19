ca74e1e26c52243720870709d40a50c645def0bc
Chizzy-glitch <chinedunwigwechizorom11@gmail.com>
2024-12-19 19:03:50 +0100

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+Name ="Killua"
+Age=12
+Student=True
+Weight=42.5
+print(type(Name))
+print(type(Age))
+print(type(Student))
+print(type(Weight))
+
+Age=str(Age)
+print(type(Age))
+
---
+++ swapping.py
@@ -0,0 +1,9 @@
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+
+temp=x
+x=y
+y=temp
+
+print(x)
+print(y)
